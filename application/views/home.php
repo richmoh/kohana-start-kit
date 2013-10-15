0244d387cb73c1d4d65a489e3899bbb34e494815
richardmohammed <richardmohammed@192.168.202.51>
2013-10-15 19:43:03 +0100

    Added flat ui test

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,13 @@
 <h1>Welcome! I am Kohana 3.0 with Bootstrap 3.0, Flat UI and HTML 5 Boilerplate</h1>
+
+<h2>See!</h2>
+
+<label class="radio">
+  <input type="radio" name="group1" value="1" data-toggle="radio">
+  Radio is off
+</label>
+
+<label class="radio">
+  <input type="radio" name="group1" value="2" data-toggle="radio" checked>
+  Radio is on
+</label>
